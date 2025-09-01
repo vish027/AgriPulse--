01aff224cb1efd1a5b9fecf4e1c44cebf599f550
vishal angadi <vishalangadi2004@gmail.com>
2025-09-01 15:46:15 +0530

    .

--- client/src/Components/AgricultureWebsite.jsx
+++ client/src/Components/AgricultureWebsite.jsx
@@ -47,6 +47,7 @@ const cardsData = [
     title: "Yield Prediction",
     description: "Predict harvests with AI-powered analytics.",
     icon: <FaChartBar size={40} className="text-white" />,
+    link:"https://crop-zeta.vercel.app/",
   },
   {
     title: "Packaging",
